0087f5e1db764ed69f885b81213249f409269254
Iury Piva <iurypiva@gmail.com>
2024-03-04 17:32:12 -0300

    successfully submited form page

--- app/components/Dialog.tsx
+++ app/components/Dialog.tsx
@@ -1,18 +1,54 @@
-export const Dialog = ({
-  children,
-  header,
-}: {
-  children: React.ReactNode;
-  header?: React.ReactNode;
-}) => {
+export const Dialog = (
+  props: React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >
+) => {
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      {header ? (
-        <div>
-          <h1 className="p-8 font-bold">{header}</h1>
-        </div>
-      ) : null}
-      <div className="p-8 bg-white rounded-lg">{children}</div>
+    <div
+      className="flex flex-col w-full rounded-lg bg-almost-white items-start justify-start overflow-hidden gap-4 min-h-[768px] relative"
+      {...props}
+    ></div>
+  );
+};
+
+export const DialogActions = (
+  props: React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >
+) => {
+  return (
+    <div className="flex flex-col items-end px-4 gap-2 w-full" {...props}></div>
+  );
+};
+
+export const DialogBody = (
+  props: React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >
+) => {
+  return (
+    <div className="w-full flex flex-col justify-center px-4 gap-2 items-start">
+      <div
+        className="w-full border border-solid border-warm-mid-gray rounded-lg p-4 flex flex-col items-center gap-4"
+        {...props}
+      ></div>
     </div>
+  );
+};
+
+export const DialogHeader = (
+  props: React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >
+) => {
+  return (
+    <div
+      className="bg-warm-light-gray w-full px-4 py-8 gap-2 flex flex-col"
+      {...props}
+    ></div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 "use client";
 import StarFilled from "@/public/star-filled.svg";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+
+import CloseIcon from "@/public/close-icon.svg";
+
 import { Button } from "./components/Button";
 import { Icons } from "./components/Icons";
 import { Input } from "./components/Input";
 import { Body, Headers } from "./components/Text";
+import {
+  Dialog,
+  DialogActions,
+  DialogBody,
+  DialogHeader,
+} from "./components/Dialog";
 
 type Inputs = {
   fullName: string;
@@ -15,37 +25,77 @@ type Inputs = {
 };
 
 export default function Home() {
-  const { control, handleSubmit } = useForm<Inputs>({
-    defaultValues: {
-      fullName: "",
-      dateOfBirth: "",
-      nickname: "",
-      email: "",
-      portfolioLink: "",
-    },
-    mode: "onChange",
-  });
+  const { control, handleSubmit, formState, setValue, reset } = useForm<Inputs>(
+    {
+      defaultValues: {
+        fullName: "",
+        dateOfBirth: "",
+        nickname: "",
+        email: "",
+        portfolioLink: "",
+      },
+      mode: "onChange",
+    }
+  );
 
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log({ data });
 
-  return (
-    <form
-      className="flex flex-col w-full rounded-lg bg-almost-white items-start justify-start overflow-hidden gap-4 min-h-[768px]"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="bg-warm-light-gray w-full px-4 py-8 gap-2 flex flex-col">
-        <div className="gap-2 flex items-center">
-          <Headers.Primary>Welcome!</Headers.Primary>
-          <StarFilled width={20} height={20} />
+  const debugFill = () => {
+    setValue("fullName", "John Doe");
+    setValue("dateOfBirth", "2000-01-01");
+    setValue("nickname", "johndoe");
+    setValue("email", "john@doe.com");
+    setValue("portfolioLink", "https://johndoe.com");
+  };
+
+  (window as any).debugFill = debugFill;
+
+  if (formState.isSubmitSuccessful) {
+    return (
+      <Dialog>
+        <div
+          className="flex flex-col items-center justify-center absolute w-8 h-8 right-6 top-6 z-10"
+          onClick={() => reset()}
+        >
+          <CloseIcon />
         </div>
-        <div>
+        <DialogHeader onClick={() => reset()}>
+          <div className="flex items-center justify-center">
+            <Player
+              autoplay
+              loop
+              src="https://lottie.host/bd72175a-8a58-414d-8e04-6aad6201b6cc/XsV2L4eBXs.json"
+              style={{ height: "80px", width: "80px" }}
+            >
+              <Controls visible={false} />
+            </Player>
+          </div>
+        </DialogHeader>
+        <DialogBody>
+          <Headers.Secondary>Thank you!</Headers.Secondary>
           <Body.Regular>
-            Please, complete the following example form.
+            Your form has been submitted successfully.
           </Body.Regular>
-        </div>
-      </div>
-      <div className="w-full flex flex-col justify-center px-4 gap-2 items-start">
-        <div className="w-full border border-solid border-warm-mid-gray rounded-lg p-4 flex flex-col items-center gap-4">
+        </DialogBody>
+      </Dialog>
+    );
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Dialog>
+        <DialogHeader>
+          <div className="gap-2 flex items-center">
+            <Headers.Primary>Welcome!</Headers.Primary>
+            <StarFilled width={20} height={20} />
+          </div>
+          <div>
+            <Body.Regular>
+              Please, complete the following example form.
+            </Body.Regular>
+          </div>
+        </DialogHeader>
+        <DialogBody>
           <Body.Bold className="w-full">About you</Body.Bold>
 
           <Input
@@ -82,23 +132,39 @@ export default function Home() {
             icon={Icons.Email}
             control={control}
             name="email"
-            rules={{ required: "This field is required." }}
+            rules={{
+              required: "This field is required.",
+              pattern: {
+                value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                message: "Invalid email address.",
+              },
+            }}
+            placeholder="email@domain.com"
+            type="email"
           />
           <Input
             icon={Icons.Link}
             control={control}
             name="portfolioLink"
-            rules={{ required: "This field is required." }}
+            rules={{
+              required: "This field is required.",
+              // https://stackoverflow.com/a/58172035
+              pattern: {
+                value:
+                  /[Hh][Tt][Tt][Pp][Ss]?:\/\/(?:(?:[a-zA-Z\u00a1-\uffff0-9]+-?)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]+-?)*[a-zA-Z\u00a1-\uffff0-9]+)*(?:\.(?:[a-zA-Z\u00a1-\uffff]{2,}))(?::\d{2,5})?(?:\/[^\s]*)?/,
+                message: "Invalid URL.",
+              },
+            }}
             placeholder="https://"
+            type="url"
           />
-        </div>
-      </div>
-
-      <div className="flex flex-col items-end px-4 gap-2 w-full">
-        <Button>
-          Submit my form <Icons.AllOpen />
-        </Button>
-      </div>
+        </DialogBody>
+        <DialogActions>
+          <Button>
+            Submit my form <Icons.AllOpen fill="#201E1C" />
+          </Button>
+        </DialogActions>
+      </Dialog>
     </form>
   );
 }
